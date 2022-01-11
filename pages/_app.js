111a35de0ffc386e622dd99f09bb8cbbffa9cedd
Kudhung <kudhung.bwi@gmail.com>
2022-01-11 13:27:04 +0700

    7

--- pages/_app.js
+++ pages/_app.js
@@ -11,7 +11,6 @@ function MyApp({ Component, pageProps }) {
         <link rel="stylesheet" href="/css/Post-Template.css" media="screen" />
         <script className="u-script" type="text/javascript" src="/js/jquery.js" defer=""></script>
         <script className="u-script" type="text/javascript" src="/js/nicepage.js" defer=""></script>
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
         <meta name="generator" content="Nicepage 3.27.0, nicepage.com" />
         <link id="u-theme-google-font" rel="stylesheet"
